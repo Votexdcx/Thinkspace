2b71d09168fe1ec7b09538ff8e7aa71d5a60dc61
Votexdcx <Votexdcx@gmail.com>
2025-10-11 15:01:12 +0100

    connect to Amazon Rds postgres database

--- backenddjango/ThinkSpace/Services/home_activities.py
+++ backenddjango/ThinkSpace/Services/home_activities.py
@@ -10,14 +10,32 @@ class HomeActivities:
   def run(self):
     print("|||||||||||||||||||||||||||||||||||||||||")
     print("|||||||||||||||||||||||||||||||||||||||||")
-    print(os.getenv("CONNECTION_URL"))
+    #print(os.getenv("CONNECTION_URL"))
     sql =query_wrap_array("""
-    SELECT * FROM activities
+       SELECT 
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+    FROM public.activities
+    LEFT JOIN public.users ON users.uuid = activities.user_uuid
+    ORDER BY activities.created_at DESC
+
     """)
 ##
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
-        json = cur.fetchall()
-        print(json)
+        json = cur.fetchone()
+        print("kqwhdkahdskjhdkajshdkjahskjdhksahdkh")
+        print("kqwhdkahdskjhdkajshdkjahskjdhksahdkh")
+        print("kqwhdkahdskjhdkajshdkjahskjdhksahdkh")
+        print(json[0])
+
     return json[0]
